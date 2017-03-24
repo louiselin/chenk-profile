93393bab64c39037bff166b4d01e6ba0a17231a5
louiselin <louise360992@gmail.com>
2017-03-24 20:37:50 +0800

    join page and home done

--- ck/routes/index.js
+++ ck/routes/index.js
@@ -8,6 +8,23 @@ router.get('/', function(req, res, next) {
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Hello World! It is me.'});
+  res.render('about', { title: 'About | Dr. Kung Chen'});
 });
+
+router.get('/research', function(req, res, next) {
+  res.render('research', { title: 'Research | Dr. Kung Chen'});
+});
+
+router.get('/course', function(req, res, next) {
+  res.render('course', { title: 'Courses | Dr. Kung Chen'});
+});
+
+router.get('/publications', function(req, res, next) {
+  res.render('publications', { title: 'Publications | Dr. Kung Chen'});
+});
+
+router.get('/join', function(req, res, next) {
+  res.render('join', { title: 'Join | Dr. Kung Chen'});
+});
+
 module.exports = router;

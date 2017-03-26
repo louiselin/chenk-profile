f8f3e110c6e90bba95b90face27d90b456e29658
louiselin <louise360992@gmail.com>
2017-03-27 01:04:11 +0800

    overflow check

--- ck/public/javascripts/style.js
+++ ck/public/javascripts/style.js
@@ -8,12 +8,14 @@ $(document).ready(function() {
         document.getElementById("intro").style.left="0";
         document.getElementById("detail").style.left="0";
         document.getElementById("nav").style.left="0";
+        document.getElementById("joinfooter").style.left="400px";
       } else {
         $(".navlist").fadeIn(1000, "linear");
         document.getElementById("intro").style.left="300px";
         document.getElementById("detail").style.left="300px";
         document.getElementById("nav").style.left="300px";
+        document.getElementById("joinfooter").style.left="700px";
       }
     });
-  
+
 });
